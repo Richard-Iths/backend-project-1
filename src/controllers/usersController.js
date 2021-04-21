803f283eabfe4309a8b14eb95253f3fb88517c65
richard-iths <richard.forsberg@iths.se>
2021-04-21 11:24:10 +0200

    refactored code in model added password compare to users model

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,9 +3,13 @@ const userModel = models.userModel;
 
 const postLogin = async (req, res, next) => {
   const { email, password } = req.body;
-  const user = await userModel.findOne({ email });
-  // user.comparePassword(password);
-  res.json({ message: "success", token: user.getToken });
+  try {
+    const user = await userModel.findOne({ email });
+    await user.comparePassword(password);
+    res.json({ message: "success", token: user.getJwtToken() });
+  } catch (error) {
+    res.json(error.message);
+  }
 };
 
 module.exports = {
--- src/models/users.js
+++ src/models/users.js
@@ -1,9 +1,22 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 module.exports = (sequelize, Sequelize) => {
   class User extends Sequelize.Model {
     toObject() {
-      delete this.id;
+      return this.email;
+    }
+    getJwtToken() {
+      return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+        expiresIn: "5d",
+      });
+    }
+    async comparePassword(password) {
+      try {
+        await bcrypt.compare(password, this.password);
+      } catch (error) {
+        throw new Error("compare failed");
+      }
     }
   }
   User.init(
@@ -22,17 +35,6 @@ module.exports = (sequelize, Sequelize) => {
     {
       sequelize,
       modelName: "User",
-      getterMethods: {
-        toObject() {
-          return { email: this.email };
-        },
-        getToken() {
-          const token = jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
-            expiresIn: "7d",
-          });
-          return token;
-        },
-      },
     }
   );
 
